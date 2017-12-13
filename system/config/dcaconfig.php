6c8051569b66fdd11c73e7e6aff48822bc156584
Andreas Kroll <andreas.kroll@yahoo.de>
2017-12-13 16:52:14 +0100

    <strong>-Tag added

--- system/config/dcaconfig.php
+++ system/config/dcaconfig.php
@@ -1,3 +1,3 @@
 <?php
-$GLOBALS['TL_DCA']['tl_content']['fields']['headline']['options'] = array('h2', 'h3', 'h4', 'h5', 'h6', 'span');
-$GLOBALS['TL_DCA']['tl_module']['fields']['headline']['options'] = array('h2', 'h3', 'h4', 'h5', 'h6', 'span');
+$GLOBALS['TL_DCA']['tl_content']['fields']['headline']['options'] = array('h2', 'h3', 'h4', 'h5', 'h6', 'span', 'strong');
+$GLOBALS['TL_DCA']['tl_module']['fields']['headline']['options'] = array('h2', 'h3', 'h4', 'h5', 'h6', 'span', 'strong');
