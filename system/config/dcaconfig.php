a056c891c1a2a3a86e9bc64238a5302c1f0a5604
Andreas Kroll <andreas.kroll@yahoo.de>
2019-03-08 15:41:43 +0100

    Update dcaconfig.php

--- system/config/dcaconfig.php
+++ system/config/dcaconfig.php
@@ -21,3 +21,4 @@ if (!\BackendUser::getInstance()->isAdmin) {
 }
 #$GLOBALS['TL_DCA']['tl_content']['fields']['text']['eval']['rte'] = 'tinyMCE_custom';
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['options']['eval']['allowHtml'] = true;
+$GLOBALS['TL_DCA']['tl_form_field']['fields']['label']['eval']['allowHtml'] = true;
