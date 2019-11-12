115f5291e05f7648b20a5410df2d852db38c84c0
Andreas Kroll <andreas.kroll@yahoo.de>
2019-11-12 17:04:26 +0100

    Create localconfig.php

---
+++ system/config/localconfig.php
@@ -0,0 +1,10 @@
+<?php
+
+### INSTALL SCRIPT START ###
+$GLOBALS['TL_CONFIG']['folderUrl'] = true;
+$GLOBALS['TL_CONFIG']['disableCron'] = true;
+$GLOBALS['TL_CONFIG']['defaultUser'] = 1;
+$GLOBALS['TL_CONFIG']['defaultGroup'] = 1;
+$GLOBALS['TL_CONFIG']['enableSearch'] = false;
+$GLOBALS['TL_CONFIG']['sessionTimeout'] = 36000;
+### INSTALL SCRIPT STOP ###
