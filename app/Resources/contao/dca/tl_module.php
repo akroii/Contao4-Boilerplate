4851898f9cdeaa822feac419f4366f2ac8a98e50
Andreas Kroll <andreas.kroll@yahoo.de>
2021-09-10 14:27:13 +0200

    fixed placeholder text

--- app/Resources/contao/dca/tl_module.php
+++ app/Resources/contao/dca/tl_module.php
@@ -1,8 +1,8 @@
 <?php
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
-$GLOBALS['TL_DCA']['tl_search']['fields']['placeholder'] = [
-    'label' => ['Placeholder', 'Platzhalter'],
+$GLOBALS['TL_DCA']['tl_module']['fields']['placeholder'] = [
+    'label' => ['Placeholder-Text', 'Welcheer Text soll als Platzhalter in dem Eingabefeld stehen?'],
     'inputType' => 'text',
     'eval' => ['tl_class' => 'w50', 'maxlength' => 255],
     'sql' => ['type' => 'string', 'length' => 255, 'default' => ''],
